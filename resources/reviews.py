df768128cd157dfe4eef4448b45f2eb754cf09b9
caroline opiyo <opiyocaroline20@gmail.com>
2024-01-26 00:29:38 +0300

    Implement logic to retrieve reviews, handling both single and multiple records

--- resources/reviews.py
+++ resources/reviews.py
@@ -1,6 +1,7 @@
 from models import ReviewModel, db
 from flask_restful import Resource,fields, marshal_with, reqparse
 
+# Define fields for marshaling responses
 review_fields = {
     "id" : fields.Integer,
     "workouts_id" : fields.Integer,
@@ -12,6 +13,8 @@ review_fields = {
     
 }
 
+
+# Request parser for handling review data in requests
 class Review(Resource):
     review_parser = reqparse.RequestParser()
     review_parser.add_argument('workouts_id', required = True,type=int,help="Workouts id is required" )
@@ -20,6 +23,7 @@ class Review(Resource):
     review_parser.add_argument('body', required = True,help="Body is required" )
     review_parser.add_argument('status', required = True,help="Description is required" )
 
+    #Retrieve review(s) based on the provided ID.
     @marshal_with(review_fields)
     def get(self,id=None):
         if id:
@@ -29,6 +33,7 @@ class Review(Resource):
             reviews = ReviewModel.query.all()
             return reviews
         
+    #Create a new review.
     def post(self):
         data = Review.review_parser.parse_args()
 
@@ -42,7 +47,7 @@ class Review(Resource):
         except:
             return {"message" : "unable to create review"}
         
-        
+     #Update review information based on the provided ID.   
     @marshal_with(review_fields)
     def patch(self,id):
         data = Review.review_parser.parse_args()
@@ -61,6 +66,7 @@ class Review(Resource):
         else:
             return {"message":"review not found"}
         
+    #Delete a review based on the provided ID.
     def delete(self,id):
         review = ReviewModel.query.get(id)
         if review:
