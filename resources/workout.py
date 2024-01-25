f72d6673eeb684100a7737973cca60ee12764184
caroline opiyo <opiyocaroline20@gmail.com>
2024-01-26 00:06:06 +0300

    Set up the Workout resource with necessary imports, request parsing, and field definitions

--- resources/workout.py
+++ resources/workout.py
@@ -1,6 +1,7 @@
 from models import WorkoutModel, db
 from flask_restful import Resource,fields, marshal_with, reqparse, marshal
 
+# Define fields for marshaling
 workout_fields = {
     "id" : fields.Integer,
     "users_id" : fields.Integer,
@@ -11,6 +12,7 @@ workout_fields = {
     "created_at" : fields.DateTime
     
 }
+# Request parser for handling input validation
 # jwt_required()
 class Workout(Resource):
     workout_parser = reqparse.RequestParser()
@@ -20,38 +22,47 @@ class Workout(Resource):
     workout_parser.add_argument('description', required = True,help="Description is required" )
     workout_parser.add_argument('time', required = True,help="Time is required" )
 
+    # GET method to retrieve workout(s)
     def get(self,id=None):
         if id:
+            # Retrieve a specific workout by ID
             workout = WorkoutModel.query.filter_by(id=id).first()
             if workout == None:
                return {"message":"workout not found"}, 404
             return marshal(workout, workout_fields)
         else:
+            # Retrieve all workouts
             workouts = WorkoutModel.query.all()
             return  marshal(workouts, workout_fields)
-        
+    
+    # POST method to create a new workout    
     def post(self):
         data = Workout.workout_parser.parse_args()
 
+        # Create a new workout instance
         workout = WorkoutModel(**data)
 
         try:
+            # Add the workout to the database and commit the changes
             db.session.add(workout)
             db.session.commit()
 
             return {"message":"Workout created successfully"}
         except:
             return {"message" : "unable to create workout"}
-        
+    
+    # PATCH method to update a workout by ID    
     @marshal_with(workout_fields)
     def patch(self,id):
         data = Workout.workout_parser.parse_args()
         workout = WorkoutModel.query.get(id)
 
+# Update the workout attributes based on the provided data
         if workout:
             for key,value in data.items():
                 setattr(workout,key,value)
             try:
+                # Commit the changes to the database
                 db.session.commit()
 
                 return {"message":"workout updated successfully"}
@@ -60,11 +71,13 @@ class Workout(Resource):
             
         else:
             return {"message":"workout not found"}
-        
+    
+    # DELETE method to delete a workout by ID    
     def delete(self,id):
         workout = WorkoutModel.query.get(id)
         if workout:
             try:
+                # Delete the workout from the database and commit the changes
                 db.session.delete(workout)
                 db.session.commit()
 
